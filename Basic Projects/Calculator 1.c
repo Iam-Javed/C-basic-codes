d1a63e46106b0e5e8c3274689858d515db395125
Javed Basha <106265348+Iam-Javed@users.noreply.github.com>
2023-08-14 16:28:18 +0530

    Update Calculator 1.c

--- Basic Projects/Calculator 1.c
+++ Basic Projects/Calculator 1.c
@@ -1,35 +1,44 @@
 #include <stdio.h>
 
 int main() {
-    char operator;
-    double num1, num2;
+    char operator; // Stores the operator (+, -, *, /)
+    double num1, num2; // Stores the two numbers
+    double result; // Stores the result of the operation
 
+    // Prompt the user for an operator
     printf("Enter an operator (+, -, *, /): ");
-    scanf("%c", &operator);
+    scanf(" %c", &operator); // Note the space before %c to consume any newline characters
 
+    // Prompt the user for two numbers
     printf("Enter two numbers: ");
     scanf("%lf %lf", &num1, &num2);
 
+    // Perform the appropriate operation based on the operator
     switch (operator) {
         case '+':
-            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            result = num1 + num2;
             break;
         case '-':
-            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            result = num1 - num2;
             break;
         case '*':
-            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            result = num1 * num2;
             break;
         case '/':
             if (num2 != 0) {
-                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+                result = num1 / num2;
             } else {
                 printf("Cannot divide by zero.\n");
+                return 1; // Exit the program with an error code
             }
             break;
         default:
             printf("Invalid operator.\n");
+            return 1; // Exit the program with an error code
     }
 
-    return 0;
+    // Display the result
+    printf("Result: %.2lf\n", result);
+
+    return 0; // Exit the program successfully
 }
